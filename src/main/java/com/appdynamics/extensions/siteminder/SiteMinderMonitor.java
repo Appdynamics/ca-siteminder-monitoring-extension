d344a71154a234d902bedc2b9595a7ce34fb0a29
Abhishek Saxena <abhishek.saxena@appdynamics.com>
2021-08-23 15:53:36 +0530

    Removed prev time and current time from siteminder monitor

--- src/main/java/com/appdynamics/extensions/siteminder/SiteMinderMonitor.java
+++ src/main/java/com/appdynamics/extensions/siteminder/SiteMinderMonitor.java
@@ -11,7 +11,6 @@ package com.appdynamics.extensions.siteminder;
 
 import com.appdynamics.extensions.ABaseMonitor;
 import com.appdynamics.extensions.TasksExecutionServiceProvider;
-import com.appdynamics.extensions.conf.MonitorContextConfiguration;
 import com.appdynamics.extensions.logging.ExtensionsLoggerFactory;
 import com.appdynamics.extensions.util.AssertUtils;
 import org.slf4j.Logger;
@@ -29,8 +28,6 @@ public class SiteMinderMonitor extends ABaseMonitor {
     private static final Logger logger = ExtensionsLoggerFactory.getLogger(SiteMinderMonitor.class);
     private static final String METRIC_PREFIX = "Custom Metrics|SiteMinder|";
     private static final String MONITOR_NAME = "Siteminder monitor";
-    private long previousTimestamp = 0;
-    private long currentTimestamp = System.currentTimeMillis();
 
     @Override
     protected String getDefaultMetricPrefix() {
@@ -47,22 +44,18 @@ public class SiteMinderMonitor extends ABaseMonitor {
         List<Map<String, ?>> instances = (List<Map<String, ?>>) getContextConfiguration().getConfigYml().get("instances");
         AssertUtils.assertNotNull(instances, "Instances cannot be null or empty in config");
         logger.debug("Started Extension");
-        previousTimestamp = currentTimestamp;
-        currentTimestamp = System.currentTimeMillis();
-        if (previousTimestamp != 0) {
-            for (Map<String, ?> instance : instances) {
-                try {
-                    SiteMinderMonitorTask task = createTask(instance, tasksExecutionServiceProvider);
-                    tasksExecutionServiceProvider.submit((String) instance.get("displayName"), task);
-                } catch (Exception e) {
-                    logger.error("Error while creating task for {}", Util.convertToString(instance.get("displayName"), ""),e);
-                }
+        for (Map<String, ?> instance : instances) {
+            try {
+                SiteMinderMonitorTask task = createTask(instance, tasksExecutionServiceProvider);
+                tasksExecutionServiceProvider.submit((String) instance.get("displayName"), task);
+            } catch (Exception e) {
+                logger.error("Error while creating task for {}", Util.convertToString(instance.get("displayName"), ""), e);
             }
         }
 
     }
 
-    private SiteMinderMonitorTask createTask(Map instance,TasksExecutionServiceProvider tasksExecutionServiceProvider) {
+    private SiteMinderMonitorTask createTask(Map instance, TasksExecutionServiceProvider tasksExecutionServiceProvider) {
         return new SiteMinderMonitorTask.Builder()
                 .instance(instance)
                 .metricPrefix(getContextConfiguration().getMetricPrefix())
